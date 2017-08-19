e2b573a3ef883ef2f5c613070d2d562848a048b0
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2017-08-19 18:14:58 -0500

    u1

--- twilio_sms/models/twiliosms_send.py
+++ twilio_sms/models/twiliosms_send.py
@@ -1,17 +1,6 @@
 ##################################    TWILIO PYTHON   ###########################################
 
 
-##################################    TWILIO PYTHON   ###########################################
-
-from twilio import *
-from odoo import models, fields, api
-from twilio.rest import TwilioRestClient
-from odoo.exceptions import Warning
-import datetime
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-from twilio import TwilioRestException
-
 # Provide account value and token on below line
 client = TwilioRestClient(account='YOUR ACCOUNT', token='YOUR TOKEN HERE')
 
