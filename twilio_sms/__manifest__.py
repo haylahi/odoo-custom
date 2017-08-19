70ba6f22819721e81a4fcee8695e9bbeb00c91d9
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2017-08-19 15:35:32 -0500

    update twilio7

--- twilio_sms/__manifest__.py
+++ twilio_sms/__manifest__.py
@@ -3,11 +3,6 @@
     'category': 'SMS app',
     'website': 'viki2.odoo.com',
     'author': 'Vignesh',
-    'images': ['images/main_screenshot.png'],
-    'depends': ['base'],
-    'data': [
-         'views/twiliosms_send_view.xml',
-    ],
     'installable': True,
     'application': True
 }
