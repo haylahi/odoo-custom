21fcf872dae9ddfe2d05b0744312b12bda24e0ba
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2017-08-19 18:50:13 -0500

    u7

--- twilio_sms/models/twiliosms_send.py
+++ twilio_sms/models/twiliosms_send.py
@@ -8,3 +8,9 @@ import datetime
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 #from twilio import TwilioRestException
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
