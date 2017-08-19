73d8af2fba02f75f214382eae9b637caaad34984
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2017-08-19 16:16:31 -0500

    update

--- twilio_sms/__manifest__.py
+++ twilio_sms/__manifest__.py
@@ -1,8 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
-    'name': 'Twilio SMS',
-    'category': 'SMS app',
-    'website': 'viki2.odoo.com',
-    'author': 'Vignesh',
+    'name' : 'Twilio SMS',
+    'author' : 'Vignesh',
+    'images': ['images/main_screenshot.png'],
+    'depends': ['base'],
+    'category' : 'SMS app',
+    'application' : True,
+    'website': 'http://viki2.odoo.com',
+    'data': [
+         'views/twiliosms_send_view.xml',
+    ],
     'installable': True,
-    'application': True
+    'application': True,
+    'active'    :   True
 }
