dcc655cdc07251150831c112a3fda39c054cb1de
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2017-08-19 18:48:21 -0500

    u2

--- twilio_sms/models/twiliosms_send.py
+++ twilio_sms/models/twiliosms_send.py
@@ -1,17 +1,10 @@
 ##################################    TWILIO PYTHON   ###########################################
 
 #from twilio import *
-from odoo import models, fields, api
+from openerp import models, fields, api
 #from twilio.rest import TwilioRestClient
-from odoo.exceptions import Warning
+from openerp.exceptions import Warning
 import datetime
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 #from twilio import TwilioRestException
-
-from twilio.rest import Client
-
-# Get these credentials from http://twilio.com/user/account
-account_sid = "ACXXXXXXXXXXXXXXXXX"
-auth_token = "YYYYYYYYYYYYYYYYYY"
-client = Client(account_sid, auth_token)
