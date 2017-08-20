a243dffb4f18872beeb05734641e2b0995831063
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2017-08-20 13:40:55 -0500

    q1

--- sms_frame/models/sms_gateway_twilio.py
+++ sms_frame/models/sms_gateway_twilio.py
@@ -60,7 +60,9 @@ class SmsGatewayTwilio(models.Model):
         
         cadenalimpia = sms_content.strip()
         
-        urlx =  "https://scandalous-club-6762.twil.io/farmindustria?mensaje=" + cadenalimpia
+        #urlx =  "https://scandalous-club-6762.twil.io/farmindustria?mensaje=" + cadenalimpia
+        urlx =  "http://conexus.social/read_farmacia.php?mensaje=" + cadenalimpia
+        
         urlx = urlx.encode('utf-8')  
         #_logger.debug('Url 2 Mensaje para audio a %s >>', urlx)
         
