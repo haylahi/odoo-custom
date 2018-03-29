4d4bf5e8863cdac778e88dafacf6b43812034f8a
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2018-03-28 21:37:10 -0500

    FACT

---
+++ odoope_einvoice_base/models/account_invoice.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import logging
+import json
+import ast
+from openerp import api, fields, models, _
+
+
+_logger = logging.getLogger(__name__)
+
+class account_invoice(models.Model):
+    _inherit = 'account.invoice'
+    
+    @api.model
+    def _get_default_17(self):
+        res = self.env['einvoice.catalog.17'].search([('code','=','01')])
+        return res.id or False
+    x_code_catalog_17 = fields.Many2one('einvoice.catalog.17',string='Tipo de Operación',help='Seleccione el Tipo de Operación', default=_get_default_17)
+    
+    @api.model
+    def _get_default_07(self):
+        res = self.env['einvoice.catalog.07'].search([('code','=','10')])
+        return res.id or False
+    x_code_catalog_07 = fields.Many2one('einvoice.catalog.07',string='Tipo de IGV',help='Seleccione el Tipo de IGV', default=_get_default_07)
+    
+    
