3c1c11ff08b5cb24a6603ceacc6381a6a0efab69
MYRCONSULTING <ricardo.livelli@myrconsulting.net>
2018-04-16 12:52:51 -0500

    factu

--- facturacion_electronica_online_myr/models/account_invoice.py
+++ facturacion_electronica_online_myr/models/account_invoice.py
@@ -5,16 +5,41 @@ import ast
 import io
 import base64
 import os.path
+import suds
 from openerp import api, fields, models, _
+from suds.client import Client
+from suds.plugin import MessagePlugin
+from lxml import etree
+from lxml.etree import Element, SubElement
 
+
+#from pysimplesoap.client import SoapClient
 
 _logger = logging.getLogger(__name__)
 
+xsdpath = os.path.dirname(os.path.realpath(__file__)).replace('/models','/static/xsd/ver_1_0/maindoc')
+
+
+class MyPlugin(MessagePlugin):
+    def marshalled(self, context):
+        _logger.debug('Cambia namespace')
+        soap_env_parent = context.envelope
+        #soap_env_parent.set('xmlns:wsse', 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd')
+        
+        #modify this line to reliably find the "recordReferences" element
+        #context.envelope[1].setPrefix('SOAP-ENV')
+        #context.envelope[1][0].setPrefix('nsenzito0')
+        #context.envelope[1][0][0].setPrefix('nsricardo0')
+
 class account_invoice(models.Model):
     _inherit = 'account.invoice'
-    
+
     file_factura_json = fields.Binary('Descargar Archivo Sunat')
     factura_binary_fname = fields.Char('Archivo Sunat')
+    
+    file_factura_xml_send = fields.Binary('Descargar Archivo Sunat')
+    factura_binary_fname_xml_send = fields.Char('Archivo Sunat')
+    
     file_factura_zip = fields.Binary('Respuesta Sunat')
     factura_binary_fname_zip = fields.Char('Archivo ZIP Sunat')
     file_factura_xml = fields.Binary('Respuesta Sunat')
@@ -186,6 +211,83 @@ class account_invoice(models.Model):
             _logger.debug('Verifique el parametro de sistema de la ruta de archivos de Sunat !')
             
     
+    
+    @api.multi
+    def generate_xml_invoice_10(self):
+        ruta_sunat = self.env['ir.config_parameter'].search([('key','=','facturacion_electronica.myr.ruta')]).value
+        lines = []
+        
+        if ruta_sunat!='':
+            for invoice in self:
+                res_partner = self.env['res.partner'].browse(invoice.partner_id)
+                num_ruc_company = invoice.company_id.vat
+                cc = invoice.journal_id.sequence_id.code
+                serie = invoice.journal_id.sequence_id.prefix
+                serie = serie[:4]
+                number_invoice = invoice.number
+                #number_invoice = number_invoice[5:]
+                #namefilexml = num_ruc_company + "-" + cc + "-" + serie + "-" + number_invoice +".xml"
+                namefilexml = "20378890161-01-F933-5883.xml"
+                 
+                _logger.debug('INVOICE 1.0 >> %s  ', invoice.partner_id)
+                
+                #with open(ruta_sunat+namefilexml, 'w') as file:
+                    #json.dump(datos, file,sort_keys=True,separators=(',',':'))
+                
+                if os.path.isfile(ruta_sunat+namefilexml):
+                    file = open(ruta_sunat+namefilexml, "rb")
+                    out = file.read()
+                    file.close()
+                    _logger.debug('XML to %s >> ', out)
+                    invoice.file_factura_xml_send = base64.encodestring(out)
+                    invoice.factura_binary_fname_xml_send = namefilexml
+                    self.write({'factura_binary_fname_xml_send': namefilexml})
+                    
+                    #Produccion
+                    #client = Client(url='https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl',plugins=[MyPlugin()])
+
+                    #Pruebas
+                    client = Client(url='https://e-beta.sunat.gob.pe/ol-ti-itcpfegem-beta/billService?wsdl',plugins=[MyPlugin()])
+                    
+                    #auth = client.factory.create('Header')
+                    #auth.token = 'xxx'
+                    #client.set_options(soapheaders=auth)
+                    
+                    security =  suds.wsse.Security()
+                    token =  suds.wsse.UsernameToken('20378890161MODDATOS', 'moddatos')
+                    #token.setnonce('<INSERT_YOUR_NONCE>') # token.setonce() didn't work for me
+                    #token.setcreated()
+                    security.tokens.append(token)
+                    client.set_options(wsse=security)
+                    
+                    #res=client.service.GetDeviceInformation()
+                    
+                    
+                    #_logger.debug('WSDLXXXX : %s ', res)
+                    lg_element = '<wsse:Security><wsse:UsernameToken><wsse:Username>20378890161MODDATOS</wsse:Username><wsse:Password>moddatos</wsse:Password></wsse:UsernameToken></wsse:Security>'
+                    #lg_element = Element('wsse:UsernameToken')
+                    #soapuser_element = Element('Username').setText('20378890161MODDATOS')
+                    #soappass_element = Element('Password').setText('moddatos')
+                    #lg_element.append(soapuser_element)
+                    #lg_element.append(soappass_element)
+                    #client.set_options(headers={'key2': 'value'})
+                    
+                    
+                    request = client.service.sendBill(fileName='20378890161-01-F933-5883.zip',contentFile='UEsDBBQAAAAIABc3iky8VZEfAQ4AAKMnAAAcAAAAMjAzNzg4OTAxNjEtMDEtRjkzMy01ODgzLnhtbO052W7qWpbvSPwDnXrpUhQ8MUZJStsjBtvgialuPRjbGAO2wQPG/FurPql+obeZAgmck5x7u65aqqPohL3XPO61yMvftt6ytLHDyA381wesjD6UbN8MLNd3Xh90jX1qPPztrfDC+5vANe0SxPaj14ck9J8DI3KjZ9/w7Og5WtmmO3VNI3YD/zmZLJ8jc2Z7xvM2sp6PtE/4w4H8OTLMA4so8Y34LouVHSaXfNQcGzhOaDtGbFOBtwp824+jJ+zE2DTMb+oGuXiBf4vpWVtz8mtMSYhu3mKYWPGBYZL/2KYN/zftqWHGz5YRGx946/46MZbwbFs0hGrZyo7UvSwxsJKl/fyuqBlHdxkHZuJBNQ48zyT2Nv4l25htbPt5ytyyb219l6l8aWGcW/jOzYI2zeJ49YwgaZqWU6IchA6CoyiKoE0E4liR6/zlhH1gKgQHOb+aqKVy+Td48ZtnuD703G86KfSYpzO4jJYh9CRyG7l3NMSQoSgcYvXk+lFswGg8vBULL9Dtz5Dn2YvRrcvT3YWvffgpzu+t6Fl1HVgOSWiXeOv14S8q2ySIp2qjQTxhDbSCVrEK3iCaD28nXNvi/WmwP1KGH/jQB0t3t/eDaMezwCqBpROEbjzzbpmjKQeLFIZ6gnY/mVjFf8pvUAKrPiBvVyp9heHHCIaR8RTNDOzIS7Gndgg7kV3SFf714WCHFhp+NA1CL7o+fk+S7W/sZbCyrafopHAuFPksgHYdO4p/xZoLSw5M+sYysd8q6Hi0WMyIWdSqUmh/wzXrNZKcNsKt/PqCfEB+QS79cDh+iOTZ4wcK2cuCrW90/KGTPkYKMCosNnwMFEMdBvXOajyLnIq3XdO7fpNT7CHXcfRBQukxnjgT02g1dkRPeHx00GkbaRULO9SZkAPeUhy64/QxddpQCbTFE2PCJjx5V9db5MJM15OONPNxxPPqUkUYg+1wKk9Mi2s2WLWTUV1O6abTuFgYp+Sk1pyTTidThoTID1ZyHZPn6aw+ZGO3wnCmX800xgsDsWurj73lyuuLu7mqcroznGtsG+2IopvFlofPioXGBO27Lp0J8UJb16ZiErYJqEB9jXI625SFGurjMbZ2ev12dZog6NKe1jkOEblJ+ljlqj0rlg020NkKypHFAo2Ig0q67jskKzoV3UhGdS/rcM2VIzdk2qScnVUJnNfXF+Szz/MwdOzsHJJhFW3mbex8oOwwPrQc+03kedbTKIrkOQekPAkcvqPTkzUpA/iPZBgNSKSzWM8WLtdMURLIOgtoipzPGUEECw5gOkPOREpGnW2xwNCgSzpSnwSBRmJWW8f7wni42PJz4BzuI43TpY3ZWmbGwAosCmQizWx59ozr6CizZedALxZOFLS+TCyun008FjUGzWQ0SLeiBjYHuKh1LqhlvJ9Z3NKz9SXDM+zC8thkhPcXxQLPSkvTH6/gacczEsnTfNqamZKomalE87ioLTKJZiqD/Z1zfTenSPzC4mIht1ln9C2tAeGgh6mRzHY1GTBbTgPDkxcYtk3qqOPoTF/tq6QmLJhEzipbYQfio31isdBejmcTbrkYDZXlmCJdWyUNu69k1lBCeVbajPDlcsyxC2hRktalxBhgM1GRU8YZ0X1ZFpg0gn4UiS7tVIsF6FGoOaOIoLHXltqKvEibFWmno13N3PI7MDv5jlus+rJecdRFU1NUUtQwkh24JKPoqSOjTbVY0BesKKp8yoNRuxOM+dnGlIC8INmZtJ7AnBgPV8D02J3RIqF/dejtZgyziuTnHzOHYYsFALoUkBtAhBiU04GfGVCvLB7FdQurhFnQWQsa91jndY1SKjYnaUjl0cTbPl5nFmOhw9d3ykwbTJQ5qgbFgsV2x2572+ZWozXXlNuEM6PX3Z0aztozfwoscaVUVo/AJSqKaW83DWkidyZor51qM9qvWd16zFJaRR/0lHhXLPi1Xg8DGzZiSA9AlSkRGzTEtTKoEW7WbQz6kl735Lkxe6TD+hafuPWK1p2MJoPHyTqa4p7WrKkoy2eD9sIvFlhMN5Q+u3ZriWn3lp7Tm6ky0hzH4NFnOv0s6QXzgWoqVW44c5T2TCEa0dYdtZlxdTpMN9sm2qsOE0daL1S7WKjU6rCp6UPwqKJdp9Nf7kg6bO+CDYLDPjMbhmFlM9hqbJiOKV/WRKkz1bB2NuvKPA1kQAYVnlzDOgeGnhYLLRnmjYJ2SXLEsO1dreZic8KvR/ajxPtcxjKb5VRqr3siqOQZZNEpQyIpDFXKMxENpnn2tFSR4WgwKBYcUhsKGbKjpuyA74tWFU0TlVE6eDBpejRo7LGVBiNpYCfSnCPUQDxz48BqKWnXbcBu0Hdh9s8F31oVCybeD2El7/NIwKVsQjWJEZ7XhjQfaXwyItqRyKEcRUVc3o3IlCFJjkmHO0CQcGwmHYYlZTMFwSgsFoyWgpp0sBEIEht529UoqxLGUFoaeL8qeNJmojbnpi9vLFxawQrbjXAmg/eZRYPFFbfRaHTBDW/OTQIkFr7cjQfx0lar8wmObkRq3x+stiy7IgUudBQp0glybgwY8QTliGKxcMGfJmmAwWo5Y7BywHRo4IkguOIigpSj3Pwsk87c7AD45FPOiC8WOumIJGW9BVI5Va5rtUfSsMdpeW92TvVIw05PAR6kNKQ+46YyBRwKwB4CTI5Uoy6MeOugw5pT+QlByyI8Q++rpzOTSwUA5heQqckq7LU910mj0YhbYIIi4IZRLHBuQIyQYYQYnjZNFQN16lVfpwWhqvLLCiV3NzPfilBs7k+JUO6q5sDzkuZuFpJUz5qiwz7FOorTGy9cclosJCuaY0lqoYCVYvGZ2CUyY9RK6rSvrdocF1pqjc9Qvz7mEEvrM+uV72+XNT5eNYnBRrWiFasgbYxtWlO2BqOgolNN9B/RdV9uN7TKZpi5nKLsqu0kmMUY+agCrLHFYidjpnV62aGlHtIeSamqMKMhGqvSoNobNplJ1jAoWAuUZjXlFHa52XrHUi1ZbNADZoB0KVKu0umOCSuPOoFuoohVfWeQmkMZycQ+t6zYq5jodcKWHu4YBrEGI6lYqGmuM0uRCufPeE4K6RfkxhP+glw98sjl8381HsDjvfEduTfrf3X+jwzzGViWm0/vxjIXHnr7Uf4zUITrWWyEmRbExjIHw332maffMDi7vyDHw/G6Z2TGZGkDL0j8uGQmYT55Zjz9+tBjpIe3RqVcbbwgnzD3Jv1E6jW4FwZwu4+za33Qj/ocJ6z/+jtFAw38Xer2865WGpUknekzJaorlRooAilLaldg1H/84+0Feaf7rNWlWOSHPvxe6JCbC12uCbzsH75cgVbh5aOFV7dHTCqJ4sA7bmS5P07IHwHvLjuvfB/9xkdRYtN5tuIo1nhCK09o9QW5hpxQD0tt/rUCFVj22zkpPtwf0enj9wjUMTf2MJgcL8hdaE5pmBd18W6AetuCM3YQZj3jlCaG+bw/8BaUcN7hL7jhKFFvNJooVrtKbORHlCeQZHhnxfafz0mn6hLQzpl1wkM+USL31YZLnQvL4OwBEMeGOfOOGZVj5IkTwix83/ZO+aPwb/d2+xfkhHESz2xvsEF+rgLyOUB5cZhmXt2u76jJarV07fDdqnNe2iGIov1SekS/FYj7qEde73X4Dqq9IPdAl5H7Yhipblcgu8MvBHJ/EUTQW1B0aEfnWs6Lsopi+Kc2pcahbcfX8kC/pAIB9BlFGZUIAkPPki+wT55040xNJpa7cfOecM3oTHcL64LB/izwInjHvkLJXRfmDPIaiLNL1I+gU627URy6ZvwmAjh5CowESjQjlESgvCBX8KPTjnxO5NfVdmwUL8gd0CkUF0yQ25E4xUuwHWPJXGms2E6ukxF/8qIC1FapB/eskloGZap89uonkrPgGzKQq6xDflYkV+BTDXy5hqq1WqVONPHav7mGvuVXET7GSv4Qq7qgdRUelBippPKqxohA/YqvTeP6/jLSd0qLAkKZIEriGJRLHIaVhK7GlEt4raQrZLkEj2qJYmgF/qKZUp8XBFAq/TcQSiwvASG/g3UpMjTYY7ag0uOupDHqX0ulvCbu1en/VYLfMf7XcvBzkkGwZmyvhj94vjfhYdUyjr8gV1gXbGCLiP8YThQcQeCGdqnj/oty+ycT4T4cPNe/buJHKedhpQ+0s+jLCQb5JAq5oQ9yw1rkkyPheR+Zm/O14Pr2eRj8wjh9A//CLmZrLpPI3dhfc/YH9C8O981m+e5s/wNjjfOomNtwGb2PoTuiWXJi7JO5lPhunEfm9UHi9Ye3E8EHtO+5tEw0f+zRvHjgowVr5WpI2pfRcj89xdBzcOA/dMwc92zV/nDXf+Wj5AusK8JPI/b17cnPP9QCOel/W/9lkOZ/Y6JmRuictT6ceMjKzEfTtzhM7BfkFuRIcM9C9LSRfMiMW3K/2XbKVexbXec9P3+UA1eIv0ONPFZ2aObDMtY4OeF0c6enHVL/3Kney2BfoLa3yuOq2EZ0fB4w9AX5AfhEDOcLxfChk9HjyvR+8/+5k/Kx7Z3E03Zkhu7eAe9vfo8X4DwBBPj281L+CYBSD8ilyvm5viT7ESdYp3dp8i3IXi7hap4rdH/RJFAcx6qXHkV+Sotc2Xks46+1lveM/tBbkCs+yK1mfLc/439Gf67Xytif1aAb/+nQv6NDE+X6H9eiL9LgCvP3KPKfJv2nN2mxq6tMKd+y/vXP/xFJhae6JUrgqQ7cCAVGoviu2i2JYMgIwrfbduMP6NtEpd5o4v+2tn2R5d/u28jxDD//L1BLAQIfABQAAAAIABc3iky8VZEfAQ4AAKMnAAAcACQAAAAAAAAAIAAAAAAAAAAyMDM3ODg5MDE2MS0wMS1GOTMzLTU4ODMueG1sCgAgAAAAAAABABgARVSZAMPQ0wHVmGsAw9DTAQDa7PvC0NMBUEsFBgAAAAABAAEAbgAAADsOAAAAAA==')
+                    #request.fileName = 
+                    #request.contentFile = 
+                
+                    
+                    
+                    #_logger.debug('WSDL : %s ', client)
+                    _logger.debug('WSDL-Response : %s ', request)
+                    
+                _logger.debug('Valor a %s with ', invoice.file_factura_xml_send)
+        else:
+            _logger.debug('Verifique el parametro de sistema de la ruta de archivos de Sunat !')
+            
+    
+    
     @api.multi
     def process_respuesta_sunat(self):
         ruta_sunat = self.env['ir.config_parameter'].search([('key','=','facturacion_electronica.myr.ruta')]).value
@@ -252,12 +354,13 @@ class account_invoice(models.Model):
     @api.multi
     def action_invoice_draft(self):
         _logger.debug('Invoice a Borrador %s with ', self._name)
+        self.generate_xml_invoice_10()
         res = super(account_invoice, self).action_invoice_draft()
         return res
     
     @api.multi
     def invoice_validate(self):
-        _logger.debug('CreateXYZ2 a %s with ', self._name)
+        _logger.debug('Invoice a Validar %s with ', self._name)
         res = super(account_invoice, self).invoice_validate()
         return res
     
